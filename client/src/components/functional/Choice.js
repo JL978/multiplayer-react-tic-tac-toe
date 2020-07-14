6b5621b84bd841a8b8a3c069a8988e3f4c4ee3cc
Jimmy Lam <JimmyLam978@gmail.com>
2020-07-14 11:16:43 -0400

    fixed issue with image alt error

--- client/src/components/functional/Choice.js
+++ client/src/components/functional/Choice.js
@@ -5,7 +5,7 @@ const Choice = ({logo, onChoice}) => {
     return (
         <>
         <div className='choice-container'>
-            <a href="/"><img src={logo} /></a>
+            <a href="/"><img src={logo} alt='React TicTacToe'/></a>
             <ChoiceButton onChoice={onChoice} type='primary' choice='new' label='Start New'/> 
             <ChoiceButton onChoice={onChoice} type='secondary' choice='join' label='Join Game'/> 
         </div>
